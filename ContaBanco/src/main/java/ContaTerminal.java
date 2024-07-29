322a8c2366bc9c2598bbfa0e08c0c90240f72d92
João Pedro C. de Albuquerque Silva <64850857+joaopedro-albuquerque@users.noreply.github.com>
2024-07-29 10:36:22 -0300

    Update ContaTerminal.java

--- ContaBanco/src/main/java/ContaTerminal.java
+++ ContaBanco/src/main/java/ContaTerminal.java
@@ -23,5 +23,7 @@ public class ContaTerminal {
 
         System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco. Sua agência é "
                 + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
-    }
+        
+        scanner.close();
+    }  
 }
